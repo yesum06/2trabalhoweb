df887b3f2a7c026fe687acb76a13bc0a034ed094
Computacao Unifev <computacao@labunifev.net.br>
2026-03-06 22:41:15 -0300

    Dashboard ok

---
+++ dash.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="dash.css">
+</head>
+
+<body>
+ <header>
+    <h1> Dashboard</h1>
+ </header>
+ <div class="dasboard-container">
+    
+ <nav>
+    <ul>
+       
+     <li><a href="#">Início</a></li>
+     <li><a href="#">Projetos</a></li>
+     <li><a href="#">Relatórios</a></li>
+     <li><a href="#">Configurações</a></li>
+      
+    </ul>
+
+     <div class="perfil-usuario">
+      <img src="https://ui-avatars.com/api/?name=Eric+Freitas&background=008080&color=fff" alt="Avatar">
+      <span> Eric Freitas</span>
+     </div>
+   
+   </nav>
+
+ <main>
+  
+    <article>
+   <section class="container-cards">
+   <h2>Módulo de Usuários</h2>
+   <p>Gerencie os acessos e permissões do sistema nesta área.</p>
+    <a href="#" class+"btn"> Acessar</a>
+    </article>
+
+     <article>
+    <section class="container-cards">
+    <h2>Relatórios de Vendas</h2>
+    <p>Acompanhe os gráficos de desempenho deste mês.</p>
+    <a href="#" class+"btn"> Acessar</a>
+    </article>
+
+    <article>
+    <section class="container-cards">
+    <h2>Configurações do Servidor</h2>
+    <p>Ajuste as portas do Apache e o banco de dados.</p>
+    <a href="#" class+"btn"> Acessar</a>
+    </article>
+
+ </main>
+
+ </div>
+   <footer>
+      <p>&copy; <?php echo date('Y'); ?> Desenvolvido na aula de Web I</p>
+   </footer>
+</body>
+</html>
