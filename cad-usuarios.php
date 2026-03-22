d5dd36a5a5c965a5c044aa05fa2aad11c88a97d2
yesum06 <simoneramos0620@gmail.com>
2026-03-22 12:45:10 -0300

    Dashboard Completo

--- cad-usuarios.php
+++ cad-usuarios.php
@@ -19,37 +19,38 @@
                     <p>Preencha os dados abaixo para registrar um novo acesso.</p>
                
 
-                <form action="processa.php" method="POST">
-                    <div class="form-group">
-                        <label for="nome">Nome Completo</label>
-                        <input type="text" id="nome" name="nome" placeholder="Digite seu Nome Completo" required>
-                    </div>
-                    
-                    <div class="form-group">
-                        <label for="email">E-mail</label>
-                        <input type="email" id="email" name="email" placeholder="Digite seu E-mail" required>
-                    </div>
-
-                    <div class="form-row">
-                         <div class="form-group flex-1">
-                            <label for="senha">Senha</label>
-                            <input type="password" id="senha" name="senha" placeholder="Digite sua Senha" required>
+                    <form action="processa.php" method="POST">
+                        <div class="form-group">
+                            <label for="nome">Nome Completo</label>
+                            <input type="text" id="nome" name="nome" placeholder="Digite seu Nome Completo" required>
                         </div>
                         
-                        <div class="form-group flex-1">
-                            <label for="nivel"> Nível </label>
-                            <select id="nivel" name="nivel">
-                                 <option value="1">Usuário</option>
-                                 <option value="2">Administrador</option>
-                            </select>
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="email" id="email" name="email" placeholder="Digite seu E-mail" required>
                         </div>
-                    </div>
 
-                    <div class="form-group">
-                        <button type="submit" class="btn-save"><i class="fa fa-floppy-disk"> </i> Finalizar Cadastro</button>
-                        <a href="dashboard.php" class="btn-cancel">Cancelar</a>
-                    </div>      
-                </form>
+                        <div class="form-row">
+                            <div class="form-group flex-1">
+                                <label for="senha">Senha</label>
+                                <input type="password" id="senha" name="senha" placeholder="Digite sua Senha" required>
+                            </div>
+                            
+                            <div class="form-group flex-1">
+                                <label for="nivel"> Nível </label>
+                                <select id="nivel" name="nivel">
+                                    <option value="1">Usuário</option>
+                                    <option value="2">Administrador</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn-save"><i class="fa fa-floppy-disk"> </i> Finalizar Cadastro</button>
+                            <a href="dashboard.php" class="btn-cancel">Cancelar</a>
+                        </div>
+             </div>           
+                 </form>
             </section> 
         </main>
     </div>
